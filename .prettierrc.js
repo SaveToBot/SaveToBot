977ba5694b0343d7ae7f2e44338ba7067f46370b
Omid Panahi <me@omidpanahi.com>
2022-10-19 20:53:53 +0330

    initiate project

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'es5',
+  endOfLine: 'lf',
+  jsxSingleQuote: true,
+  printWidth: 120,
+  arrowParens: 'avoid',
+  tabWidth: 2,
+  parser: 'typescript',
+}
